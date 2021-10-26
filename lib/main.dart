8989bb431c820b4a2fadd73a6b038f7967260c0f
Altamash250996 <altamashshaikh6889@gmail.com>
2021-10-26 13:00:08 +0530

    google maps is completed

--- lib/main.dart
+++ lib/main.dart
@@ -20,3 +20,66 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+ 
+
+/* import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:geocoder/geocoder.dart';
+import 'package:google_maps_webservice/places.dart';
+
+void main() => runApp(MyApp());
+
+const kGoogleApiKey = "My key";
+
+GoogleMapsPlaces _places = GoogleMapsPlaces(apiKey: kGoogleApiKey);
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: demo(),
+      ),
+    );
+  }
+}
+
+class demo extends StatefulWidget {
+  @override
+  demoState createState() => new demoState();
+}
+
+class demoState extends State<demo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            alignment: Alignment.center,
+            child: RaisedButton(
+              onPressed: () async {
+                Prediction p = await PlacesAutocomplete.show(
+                    context: context, apiKey: kGoogleApiKey);
+                displayPrediction(p);
+              },
+              child: Text('Find address'),
+            )));
+  }
+
+  Future<Null> displayPrediction(Prediction p) async {
+    if (p != null) {
+      PlacesDetailsResponse detail =
+          await _places.getDetailsByPlaceId(p.placeId);
+
+      var placeId = p.placeId;
+      double lat = detail.result.geometry.location.lat;
+      double lng = detail.result.geometry.location.lng;
+
+      var address = await Geocoder.local.findAddressesFromQuery(p.description);
+
+      print(lat);
+      print(lng);
+    }
+  }
+}
+ */
--- lib/widgets/geo_location/geo_location_screen.dart
+++ lib/widgets/geo_location/geo_location_screen.dart
@@ -1,11 +1,14 @@
 // ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_constructors_in_immutables, unused_element, unused_field, unused_local_variable, sized_box_for_whitespace, avoid_print, avoid_unnecessary_containers, prefer_collection_literals
 
+import 'dart:math';
+
 import 'package:clover_application/blocs/application_bloc.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:provider/provider.dart';
+import 'package:geocoding/geocoding.dart';
 
 class GeoLocationScreen extends StatefulWidget {
   @override
@@ -14,6 +17,9 @@ class GeoLocationScreen extends StatefulWidget {
 
 class _GeoLocationScreenState extends State<GeoLocationScreen> {
   List<Marker> myMarker = [];
+  GoogleMapController mapController;
+  String searchAddr;
+
   @override
   Widget build(BuildContext context) {
     final applicationBloc = Provider.of<ApplicationBloc>(context);
@@ -25,28 +31,20 @@ class _GeoLocationScreenState extends State<GeoLocationScreen> {
                 child: CircularProgressIndicator(),
               )
             : Container(
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Search Location',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(6.0),
-                        ),
-                        fillColor: Colors.white,
-                        filled: true,
-                      ),
-                    ),
+                child: Stack(
+                  children: <Widget>[
                     Container(
                       height: 300,
                       child: GoogleMap(
+                        onMapCreated: onMapCreated,
                         mapType: MapType.normal,
                         myLocationEnabled: true,
                         initialCameraPosition: CameraPosition(
                           target: LatLng(
-                              applicationBloc.currentLocation.latitude,
-                              applicationBloc.currentLocation.longitude),
-                          zoom: 16,
+                            applicationBloc.currentLocation.latitude,
+                            applicationBloc.currentLocation.longitude,
+                          ),
+                          zoom: getZoomLevel(25),
                         ),
                         scrollGesturesEnabled: true,
                         zoomGesturesEnabled: true,
@@ -60,11 +58,70 @@ class _GeoLocationScreenState extends State<GeoLocationScreen> {
                         onTap: _handleTap,
                       ),
                     ),
+                    Positioned(
+                      top: 10.0,
+                      right: 55.0,
+                      left: 0.0,
+                      child: Container(
+                        height: 50.0,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          color: Colors.white,
+                        ),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Enter Address',
+                            border: InputBorder.none,
+                            contentPadding:
+                                EdgeInsets.only(left: 15.0, top: 15.0),
+                            suffixIcon: IconButton(
+                              onPressed: searchNavigate,
+                              icon: Icon(
+                                Icons.search,
+                              ),
+                              iconSize: 30.0,
+                            ),
+                          ),
+                          onChanged: (val) {
+                            setState(() {
+                              searchAddr = val;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               )
       ],
     );
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  double getZoomLevel(double radius) {
+    double zoomLevel = 11;
+    if (radius > 0) {
+      double radiusElevated = radius + radius / 2;
+      double scale = radiusElevated / 500;
+      zoomLevel = 16 - log(scale) / log(2);
+    }
+    zoomLevel = num.parse(zoomLevel.toStringAsFixed(2));
+    return zoomLevel;
+  }
+
+  searchNavigate() {
+    locationFromAddress(searchAddr).then((result) {
+      mapController.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
+        target: LatLng(result[0].latitude, result[0].longitude),
+        zoom: getZoomLevel(25),
+      )));
+    });
   }
 
   _handleTap(LatLng tappedPoint) {
